264583d2cb9a5dd71665a654658671e86fdc7d70
Nischit Shetty <njmshetty@gmail.com>
2021-02-06 13:00:22 +0530

    link changes

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,3 +1,4 @@
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 import "./Checkout.css";
 import CheckoutProduct from './CheckoutProduct';
@@ -5,7 +6,8 @@ import { useStateValue } from './StateProvider';
 import Subtotal from './Subtotal';
 
 function Checkout() {
-    const[{basket}, dispatch] = useStateValue();
+    const[{basket, user}, dispatch] = useStateValue();
+
 
     return (
         <div className="checkout">
@@ -13,6 +15,7 @@ function Checkout() {
                 <img className="checkout__ad" src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt=""/>
 
                 <div>
+                <h3>Hello, {user?.email}</h3>
                 <h2 className="checkout__title">Your shopping basket</h2>
 
                 {basket.map(item =>(
--- src/Login.js
+++ src/Login.js
@@ -36,7 +36,7 @@ function Login() {
     return (
         <div className='login'>
             <Link to='/'>
-            <img className="login__logo"src='https:/upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png' alt='amazon-logo' />
+            <img className="login__logo"src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png" alt='amazon-logo' />
             </Link>
 
             <div className="login__container">
